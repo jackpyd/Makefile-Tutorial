73f801220c1b5463dbcd98638ecaa2797a97d8d1
jackc <78083634@qq.com>
2024-01-05 21:34:07 +0800

    create:CMake

---
+++ X1.CMake/02CMake的使用/demo2/build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/jackc/Desktop/git-projects/Makefile-Tutorial/X1.CMake/02CMake的使用/demo2/add.c" "CMakeFiles/app.dir/add.c.o" "gcc" "CMakeFiles/app.dir/add.c.o.d"
+  "/Users/jackc/Desktop/git-projects/Makefile-Tutorial/X1.CMake/02CMake的使用/demo2/div.c" "CMakeFiles/app.dir/div.c.o" "gcc" "CMakeFiles/app.dir/div.c.o.d"
+  "/Users/jackc/Desktop/git-projects/Makefile-Tutorial/X1.CMake/02CMake的使用/demo2/main.c" "CMakeFiles/app.dir/main.c.o" "gcc" "CMakeFiles/app.dir/main.c.o.d"
+  "/Users/jackc/Desktop/git-projects/Makefile-Tutorial/X1.CMake/02CMake的使用/demo2/sub.c" "CMakeFiles/app.dir/sub.c.o" "gcc" "CMakeFiles/app.dir/sub.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
